485728309c58924691f579ae6d7aa55cb582f17c
Enrico <e.giacomazzi@contralto-audio.com>
2025-11-19 10:16:53 +0100

    axios

--- src/services/todoService.js
+++ src/services/todoService.js
@@ -1,19 +1,15 @@
+import axios from 'axios';
+
 
 const baseUrl = 'http://localhost:3000/todos';
 
 export async function getTodos() {
-    const res = await fetch(baseUrl);
-    const data = await res.json();
-    return data;
+    const res = await axios.get(baseUrl);
+    return res.data;
 } 
 
 export async function addTodo(label) {
     const newTodo = {text: label, done: false};
-    const res = await fetch(baseUrl, {
-        method: 'POST',
-        body: JSON.stringify(newTodo)
-    });
-
-    const created = await res.json();
-    return created;
-  }
+    const res = await axios.post(baseUrl, newTodo);
+    return res.data;
+} 
